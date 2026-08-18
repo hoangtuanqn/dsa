72987a222b1b9b09cf41588d7f5944a0a0f0f7e1
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-18 07:12:08 +0700

    feat: solve problem 27 leetcode using two pointers

---
+++ leetcode/27_remove_element_using_two_pointer.cpp
@@ -0,0 +1,34 @@
+// 27. Remove Element
+// Sử dụng 2 con trỏ
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+using namespace std;
+
+int removeElement(vector<int> &nums, int val)
+{
+    int count = 0, k = 0;
+    FOR(i, 0, nums.size())
+    {
+        if (nums[i] != val)
+        {
+            nums[k] = nums[i];
+            k++;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    return nums.size() - count;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    vector<int> a = {3, 2, 2, 3};
+    removeElement(a, 3);
+
+    return 0;
+}
