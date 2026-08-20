c4fb493ca302e34c088ad67f620b258c497d249c
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-21 03:20:00 +0700

    feat: solution 509 optimize leetcode

--- leetcode/509_fibonacci_number_using_memoization.cpp
+++ leetcode/509_fibonacci_number_using_memoization.cpp
@@ -3,20 +3,26 @@ using namespace std;
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define ll long long
 #define vii vector<int, int>
-int fib(int n)
+int finonacci(int n, vector<int> &dp)
 {
-    // sử dụng đệ quy, có 1 bài sử dụng Memoization
     if (n == 0)
         return 0;
     if (n == 1)
         return 1;
-    return fib(n - 1) + fib(n - 2);
+    if (dp[n] != -1)
+        return dp[n];
+    return dp[n] = finonacci(n - 1, dp) + finonacci(n - 2, dp);
+}
+int fib(int n)
+{
+    vector<int> dp(n + 1, -1);
+    return finonacci(n, dp);
 }
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    cout << fib(3);
+    cout << fib(4);
     return 0;
 }
