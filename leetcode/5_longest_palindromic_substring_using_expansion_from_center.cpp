1a6db6bcc054f49f3ed4830dd9e6aa8f5b6e51c6
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-08-26 05:16:47 +0700

    feat: solution 5 leetcode using expansion from center

---
+++ leetcode/5_longest_palindromic_substring_using_expansion_from_center.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define ll long long
+#define vii vector<int, int>
+string longestPalindrome(string s) {
+    int startI = 0, maxLength = 1;
+    int n = s.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j <= 1; ++j) {
+            int l = i, r = i + j;
+            while (l >= 0 && r < n && s[l] == s[r]) {
+                if (r - l + 1 > maxLength) {
+                    maxLength = r - l + 1;
+                    startI = l;
+                }
+                --l;
+                ++r;
+            }
+        }
+    }
+    return s.substr(startI, maxLength);
+}
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s = "AAABCCD";
+    cout << longestPalindrome(s);
+
+    return 0;
+}
