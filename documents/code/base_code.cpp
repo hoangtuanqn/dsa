f8ddc0d9f2441f78f3eebbeaa11f72026060ddfa
htuanqn <phamhoangtuanqn@gmail.com>
2026-08-31 08:42:36 +0700

    feat: base code cpp

---
+++ documents/code/base_code.cpp
@@ -0,0 +1,54 @@
+#pragma GCC optimize("O2")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+using namespace std;
+
+// Types
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+
+// Vectors
+#define vi vector<int>
+#define vl vector<ll>
+#define vii vector<pii>
+#define vb vector<bool>
+
+// Loops
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define RFOR(i, a, b) for (int i = (a); i >= (b); --i)
+#define each(x, v) for (auto& x : v)
+
+// Shortcuts
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+// Utils
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define dbg(x) cerr << #x << " = " << x << "\n"
+
+// Constants
+const int INF = 1e9 + 7;
+const ll LINF = 1e18;
+const int MOD = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int x = 200;
+    int arr[20] = {0};
+    FOR(i, 0, 20) {
+        dbg(arr[i]);
+    }
+    return 0;
+}
